e8c0d92ccaef010430993d45e87922953db9d4a6
KMASAHIRO <k.masa1215@gmail.com>
2022-05-28 14:26:28 +0900

    パッケージ化しました

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='RESCO',
+    version='0.0.1',
+    packages=['RESCO',],
+    install_requires=open('requirements.txt').read().splitlines(),
+    url='https://github.com/KMASAHIRO/RESCO',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description='Evaluating Traffic Light RL Controller using RESCO benchmark.'
+)
