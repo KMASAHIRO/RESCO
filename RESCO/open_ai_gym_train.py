d9e4dd1c528c349c4b76d93d0f260d0299594126
KMASAHIRO <k.masa1215@gmail.com>
2022-11-02 03:28:08 +0900

    報酬を修正

--- RESCO/open_ai_gym_train.py
+++ RESCO/open_ai_gym_train.py
@@ -14,7 +14,7 @@ def train_agent_gym(
     env_name, model_save_path=None, episode_per_learn=10, episodes=1400,  max_steps=200, num_layers=1, 
     num_hidden_units=128, lr=3e-5, decay_rate=0.01, temperature=1.0, noise=0.0, encoder_type="fc", 
     lstm_len=5, embedding_type="random", embedding_num=5, embedding_decay=0.99, eps=1e-5, beta=0.25, 
-    embedding_no_train=False, embedding_start_train=None, gamma=0.99, log_dir="./", learn_curve_csv=None, 
+    embedding_no_train=False, embedding_start_train=None, gamma=1.0, log_dir="./", learn_curve_csv=None, 
     save_actions=False, device="cpu", gui=False):
     
     env = gym.make(env_name)
@@ -72,8 +72,6 @@ def train_agent_gym(
                     reward = (1.0 + obs[0])**2
                 else:
                     reward = 0.0
-            elif env_name == "CartPole-v1":
-                reward = -((1.0 + abs(obs[0]/4.8))**2 + (1.0 + abs(obs[2]/0.418))**2)
             
             if gui:
                 env.render()
@@ -91,7 +89,8 @@ def train_agent_gym(
                 R = 0
                 for k in range(len(episode_reward)):
                     R = episode_reward[-(k+1)] + gamma*R
-                    episode_reward[-(k+1)] = R
+                for k in range(len(episode_reward)):
+                    episode_reward[k] = R
                 agent.set_rewards(episode_reward)
                 break
 
@@ -136,7 +135,7 @@ def train_PPO_gym(
     num_hidden_units=128, lr=3e-5, decay_rate=0.01, temperature=1.0, noise=0.0, encoder_type="fc", 
     lstm_len=5, embedding_type="random", embedding_num=5, embedding_decay=0.99, eps=1e-5, beta=0.25, 
     update_interval=1024, minibatch_size=256, epochs=4, embedding_no_train=False, embedding_start_train=None, 
-    gamma=0.99, log_dir="./", learn_curve_csv=None, 
+    gamma=1.0, log_dir="./", learn_curve_csv=None, 
     model_type="original", save_actions=False, device="cpu", gui=False
     ):
 
@@ -215,8 +214,6 @@ def train_PPO_gym(
                     reward = (1.0 + obs[0])**2
                 else:
                     reward = 0.0
-            elif env_name == "CartPole-v1":
-                reward = -((1.0 + abs(obs[0]/4.8))**2 + (1.0 + abs(obs[2]/0.418))**2)
             
             if gui:
                 env.render()
@@ -231,10 +228,9 @@ def train_PPO_gym(
                 R = 0
                 for k in range(len(episode_data)):
                     R = episode_data[-(k+1)][1] + gamma*R
-                    episode_data[-(k+1)][1] = R
                 
                 for k in range(len(episode_data)):
-                    agent.observe({"main":episode_data[k][0]}, {"main":episode_data[k][1]}, episode_data[k][2], episode_data[k][3])
+                    agent.observe({"main":episode_data[k][0]}, {"main":R}, episode_data[k][2], episode_data[k][3])
 
                 break
         
